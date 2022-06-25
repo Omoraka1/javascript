e0e63b8cc4e24e87a58a970a9f8947f7b46352d8
Omoraka1 <105812118+Omoraka1@users.noreply.github.com>
2022-06-25 15:00:41 +0100

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,21 @@
+//var firstName='RUTH';
+//var height='5ft';
+//var country='NIGERIA';
+
+let firstName = "Ruth";
+
+let height = "5ft";
+
+let country = "Nigeria";
+
+// alert(firstName);
+// alert(height);
+// alert(country);
+
+// console.log(firstName)
+// console.log(height)
+// console.log(country)
+
+document.writeln("My Name is :"+firstName);
+document.write("My Height :"+height+ "\n");
+document.writeln("My country is :"+country);
